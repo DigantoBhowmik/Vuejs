b0e82ca2f8e3d2b3654fd1829322db1abf577f6b
Diganto Bhowmik <digantobhowmik@gmail.com>
2021-05-03 12:52:41 +0600

    first commit

---
+++ Method/app.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        count: 0,
+    },
+    method:{
+        increase: function(){
+            this.count=this.count+1;
+        },
+
+        decrease: function(){
+            this.count=this.count-1;
+        }
+    }
+});
+
+
+
