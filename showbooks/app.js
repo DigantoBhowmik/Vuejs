9728664eee833e26dd6531ba9b8f5a9e3d7d2b53
Diganto Bhowmik <digantobhowmik@gmail.com>
2021-05-06 13:33:29 +0600

    5th commit

--- showbooks/app.js
+++ showbooks/app.js
@@ -2,13 +2,18 @@ var app = Vue.createApp({
     data(){
         return{
             showbook:true,
-            name:'The power of habit',
-            author: 'charles'
+            books:[
+                {name:'The power of habit',author: 'charles',isFav:false},
+                {name:'The power of habit',author: 'charles',isFav:false},
+            ]
         }
     },
     methods:{
         hidebook(){
             this.showbook=!this.showbook
+        },
+        click1(book){
+            book.isFav=!book.isFav
         }
 
     }
